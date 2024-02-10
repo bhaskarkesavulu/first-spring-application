750116e1a542b6fa23f220e5110a90daeaeeb3bd
bhaskarkesavulu <bhaskar120689@gmail.com>
2024-02-10 10:40:03 +0530

    Spring Basic Application.

---
+++ src/Test.java
@@ -0,0 +1,19 @@
+import com.student.Student;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Test {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hello and welcome!");
+        Resource resource = new ClassPathResource("applicationContext.xml");
+        BeanFactory factory = new XmlBeanFactory(resource);
+        Student student = (Student) factory.getBean("studentbean");
+        student.displayInfo();
+        }
+    }
